464925784713d45717e28e4646f46745ad1ae725
swolepenguin <ashtonm223@gmail.com>
2020-10-28 09:46:13 -0400

    javascript test

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-const steamed = document.getElementById('steamed')
-steamed.addEventListener('hover',)
+//const steamed = document.getElementById('steamed')
+//steamed.addEventListener('hover',)
+const body = document.querySelector('body');
+
+
+const newDiv = document.createElement('div');
+newDiv.classList.add('test-div');
+
+const newP = document.createElement('p');
+newP.setAttribute('for','testing');
+newP.textContent = 'this is a javascript test';
+
+newDiv.appendChild(newP);
+
+body.appendChild(newDiv);
